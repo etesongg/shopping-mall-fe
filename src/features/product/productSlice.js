1a9805c88d23032dc334d37a694b55984d10f344
etesongg <etesong0114@gmail.com>
2024-10-28 17:38:03 +0900

    페이지네이션

--- src/features/product/productSlice.js
+++ src/features/product/productSlice.js
@@ -9,7 +9,7 @@ export const getProductList = createAsyncThunk(
     try {
       const response = await api.get("/product", { params: { ...query } });
       if (response.status !== 200) throw new Error(response.message);
-      return response.data.data;
+      return response.data;
     } catch (e) {
       return rejectWithValue(e.message);
     }
@@ -98,8 +98,9 @@ const productSlice = createSlice({
       })
       .addCase(getProductList.fulfilled, (state, action) => {
         state.loading = false;
-        state.productList = action.payload;
+        state.productList = action.payload.data;
         state.error = "";
+        state.totalPageNum = action.payload.totalPageNum;
       })
       .addCase(getProductList.rejected, (state, action) => {
         state.loading = false;
--- src/page/AdminProductPage/AdminProductPage.js
+++ src/page/AdminProductPage/AdminProductPage.js
@@ -70,7 +70,9 @@ const AdminProductPage = () => {
   };
 
   const handlePageClick = ({ selected }) => {
+    // selected 현재 페이지 selected + 1
     //  쿼리에 페이지값 바꿔주기
+    setSearchQuery({ ...searchQuery, page: selected + 1 });
   };
 
   // searchbox에서 검색어를 읽어온다 => 엔터 => searchQuery객체가 업데이트 됨 {name: 스트레이트 팬츠}
@@ -101,7 +103,7 @@ const AdminProductPage = () => {
           nextLabel="next >"
           onPageChange={handlePageClick}
           pageRangeDisplayed={5}
-          pageCount={100}
+          pageCount={totalPageNum} // 전체페이지
           forcePage={searchQuery.page - 1}
           previousLabel="< previous"
           renderOnZeroPageCount={null}
