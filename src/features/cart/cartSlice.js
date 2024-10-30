09893dca8371ec35fad609d49233bb9aab3e28fc
etesongg <etesong0114@gmail.com>
2024-10-31 01:19:30 +0900

    토스트 메시지 내용 수정

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -28,7 +28,7 @@ export const addToCart = createAsyncThunk(
     } catch (e) {
       dispatch(
         showToastMessage({
-          message: e.message,
+          message: "카트에 아이템 추가 실패",
           status: "error",
         })
       );
